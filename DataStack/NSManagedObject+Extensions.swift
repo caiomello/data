6cca86e1950d4045de4a0c9012095e7797447b6d
Caio Mello <github@caiomello.com>
2019-07-12 17:43:28 -0300

    Revert "Remove NSManagedObject extensions"
    This reverts commit f1d0e3d066296227d992bf05595d7138a960f6f8.

---
+++ DataStack/NSManagedObject+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  NSManagedObject+Extensions.swift
+//  DataStack
+//
+//  Created by Caio on 6/27/19.
+//  Copyright © 2019 Caio Mello. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension NSManagedObject {
+    public class func fetch<T: NSManagedObject>(withPredicate predicate: NSPredicate? = nil, sortDescriptors: [NSSortDescriptor]? = nil, context: NSManagedObjectContext) -> [T] {
+        let fetchRequest = NSFetchRequest<T>(entityName: T.entity().name!)
+        fetchRequest.predicate = predicate
+        fetchRequest.sortDescriptors = sortDescriptors
+
+        do {
+            return try context.fetch(fetchRequest)
+        } catch {
+            print("Data - Failed to execute fetch request: \(error)")
+            return []
+        }
+    }
+}
